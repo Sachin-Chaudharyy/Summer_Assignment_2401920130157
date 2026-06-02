f567ba3da1cd2426bb1f4bbca5b9a584eafa24f0
Sachin Chaudhary <sachinchuadhary45@gmail.com>
2026-06-02 17:08:17 +0530

    Add ContainsDuplicate class to check for duplicates

---
+++ Week_1/Day_2/Contains_Duplicate.java
@@ -0,0 +1,13 @@
+class Prob2 {
+  public boolean containsDuplicate(int[] arr) {
+    HashSet<Integer> set = new HashSet<>();
+    for(int i: arr) {
+      if(set.contains(i)){
+        return true;
+      }
+      set.add(i);
+    }
+    return false;
+  }
+}
+    
