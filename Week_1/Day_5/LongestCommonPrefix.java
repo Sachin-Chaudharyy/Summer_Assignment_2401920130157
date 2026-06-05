410186c8f55838f7d498bc33a2a8fe303d9cbf9e
Sachin Chaudhary <sachinchuadhary45@gmail.com>
2026-06-05 13:12:12 +0530

    Implement longest common prefix algorithm

---
+++ Week_1/Day_5/LongestCommonPrefix.java
@@ -0,0 +1,14 @@
+class Solution {
+  public String longestCommonPrefix(String[] strs) {
+    String s = strs[0];
+    for(int i=0; i<s.length(); i++) {
+      char c = s.charAt(i);
+      for(int j=1; j<strs.length; j++) {
+        if(i == strs[j].length() || strs[j].charAt(i) != c) {
+          return s.substring(0,i);
+        }
+      }
+    }
+    return s;
+  }
+}
