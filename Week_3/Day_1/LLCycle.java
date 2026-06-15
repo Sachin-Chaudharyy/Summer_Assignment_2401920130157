d1a760ae065f54529181097b8032e2277aca5f05
Sachin Chaudhary <sachinchuadhary45@gmail.com>
2026-06-15 12:47:57 +0530

    Implement cycle detection in a linked list

---
+++ Week_3/Day_1/LLCycle.java
@@ -0,0 +1,17 @@
+class Solution {
+  public boolean hasCycle(ListNode head) {
+    if(head == null) {
+      return false;
+    }
+    ListNode slow = head;
+    ListNode fast = head;
+    while(fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if(slow == fast) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
