f194da76a4d39a5129c7264b17dd43df108f4ec7
Sachin Chaudhary <sachinchuadhary45@gmail.com>
2026-06-17 16:14:32 +0530

    Create MinStack.java

---
+++ Week_3/Day_3/MinStack.java
@@ -0,0 +1,33 @@
+class MinStack {
+    Deque<int[]> queue;
+
+    public MinStack() {
+        queue = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        int min = queue.isEmpty() ? val : Math.min(queue.peek()[1], val);
+        queue.push(new int[] {val, min});
+    }
+    
+    public void pop() {
+        queue.pop();
+    }
+    
+    public int top() {
+        return queue.peek()[0];
+    }
+    
+    public int getMin() {
+        return queue.peek()[1];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
