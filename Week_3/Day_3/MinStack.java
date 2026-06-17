8a79a4aa8dee25f4f8adf2f130a5bf9f11af6913
Sachin Chaudhary <sachinchuadhary45@gmail.com>
2026-06-17 16:18:11 +0530

    Clean up MinStack.java by removing comments
    Removed unnecessary comments and formatting.

--- Week_3/Day_3/MinStack.java
+++ Week_3/Day_3/MinStack.java
@@ -22,12 +22,3 @@ class MinStack {
         return queue.peek()[1];
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
