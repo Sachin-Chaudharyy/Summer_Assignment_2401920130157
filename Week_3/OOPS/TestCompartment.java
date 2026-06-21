35feace1b2f5912806658f721f1c7961e3aa16e1
Sachin Chaudhary <sachinchuadhary45@gmail.com>
2026-06-21 19:24:29 +0530

    Refactor TestCompartment.java

--- Week_3/OOPS/TestCompartment.java
+++ Week_3/OOPS/TestCompartment.java
@@ -1,25 +1,25 @@
 import java.util.Random;
 
-abstract class Compartment {
+abstract class Compartment{
     public abstract String notice();
 }
-class FirstClass extends Compartment {
-    public String notice() {
+class FirstClass extends Compartment{
+    public String notice(){
         return "First Class Compartment: Reserved for AC First Class ticket holders only";
     }
 }
-class Ladies extends Compartment {
-    public String notice() {
+class Ladies extends Compartment{
+    public String notice(){
         return "Ladies Compartment: Reserved for women passengers only";
     }
 }
-class General extends Compartment {
-    public String notice() {
+class General extends Compartment{
+    public String notice(){
         return "General Compartment: Open for all passengers";
     }
 }
-class Luggage extends Compartment {
-    public String notice() {
+class Luggage extends Compartment{
+    public String notice(){
         return "Luggage Compartment: For parcels and goods";
     }
 }
@@ -28,11 +28,10 @@ public class TestCompartment {
     public static void main(String[] args) {
         Compartment[] coaches = new Compartment[10];
         Random rand = new Random();
-
-        for (int i = 0; i < coaches.length; i++) {
+        for(int i=0; i<coaches.length; i++){
             int type = rand.nextInt(4) + 1; 
 
-            switch (type) {
+            switch(type){
                 case 1:
                     coaches[i] = new FirstClass();
                     break;
@@ -48,7 +47,7 @@ public class TestCompartment {
             }
         }
         System.out.println("Train Coach Notices:\n");
-        for (int i=0; i <coaches.length; i++) {
+        for(int i=0; i<coaches.length; i++){
             System.out.println("Coach " + (i + 1) + coaches[i].notice());
         }
     }
