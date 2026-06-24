4027559725aef291b3f165f8ac6e903db0d82b2b
Sachin Chaudhary <sachinchuadhary45@gmail.com>
2026-06-24 15:58:11 +0530

    Implement searchBST method for binary search tree

---
+++ Week_4/Day_3/SearchInBST.java
@@ -0,0 +1,14 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null){
+            if(root.val < val){
+                root = root.right;
+            }else if(root.val > val){
+                root = root.left;
+            }else{
+                return root;
+            }
+        }
+        return null;
+    }
+}
